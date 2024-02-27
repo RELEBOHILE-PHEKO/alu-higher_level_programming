5cf33d00b9d2486b311087f60a4d7e24b1cbc52a
Relebohile.P <r.pheko@alustudent.com>
2024-02-27 03:45:17 -0800

    6. Inventing is a combination of brains and materials

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_number in range(0, 10):
     for second_number in range(first_number + 1, 10):
         if first_number == 8 and second_number == 9:
-            print("{}{}".format(first_number, second_number))
+            print("{:02d}".format(first_number * 10 + second_number))
         else:
-            print("{}{},".format(first_number, second_number),end='')
+            print("{:02d},".format(first_number * 10 + second_number), end='')
