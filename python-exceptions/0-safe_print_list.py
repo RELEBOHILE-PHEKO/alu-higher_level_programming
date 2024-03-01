284514007dda8bbf21392710882796e4bd9a1177
Relebohile.P <r.pheko@alustudent.com>
2024-03-01 14:33:56 -0800

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end=" ")
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")  # Print element without any space
             count += 1
-    except IndexError:
-        pass
-    finally:
-        print()
+        except IndexError:
+            break
+    print("")  # Print a newline after printing all elements
     return count
