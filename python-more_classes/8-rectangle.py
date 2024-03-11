8d0d53fb32cc2fcc8d3f06d82d66883f989c92f9
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2024-03-12 01:23:38 +0200

    A square is a rectangle

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """
+    Class that defines properties of rectangle by: (based on 7-rectangle.py).
+
+    Attributes:
+        width (int): width of the rectangle.
+        height (int): height of the rectangle.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Creates new instances of Rectangle.
+
+        Args:
+            width (int, optional): width of rectangle. Defaults to 0.
+            height (int, optional): height of rectangle. Defaults to 0.
+        """
+        self.height = height
+        self.width = width
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Width retriver.
+
+        Returns:
+            int: the width of the rectangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Height retriver.
+
+        Returns:
+            int: the height of the rectangle.
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Property setter for width of rectangle.
+
+        Args:
+            value (int): width of the rectangle.
+
+        Raises:
+
