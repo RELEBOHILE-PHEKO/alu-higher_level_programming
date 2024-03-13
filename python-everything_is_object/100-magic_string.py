84b0e4e79c1a3cee5d6584329be4608971732d80
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2024-03-13 21:14:25 +0200

    low cost memory

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    return ', '.join(['BestSchool'] * (magic_string.counter + 1))
-magic_string.counter = -1
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
