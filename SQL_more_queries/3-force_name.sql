d57e6630e4099531023751101e1fbc478dabf0ae
Relebohile.P <r.pheko@alustudent.com>
2024-03-19 09:41:29 -0700

    creates table force_name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,2 @@
--- creates a user and password with select privilages
-CREATE database IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- creates a table force_name
+CREATE table IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
