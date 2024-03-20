4e44c5785d7e3630e84f11ebce5b8e6e203b0e4d
Relebohile.P <r.pheko@alustudent.com>
2024-03-20 16:26:11 +0000

    curl headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a DELETE request to the URL passed and display body of response
-curl -s -X "POST" "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# script that takes URL as arg sends GET request with additional info
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
