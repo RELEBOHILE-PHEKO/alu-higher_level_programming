cbba41614fd53c5a2b4f2461248d16b138d23348
Relebohile.P <r.pheko@alustudent.com>
2024-03-21 17:10:47 +0000

    What is my status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     r = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))i
+    print("\t- content: {}".format(r.text))
