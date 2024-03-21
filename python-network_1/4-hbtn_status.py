a18bde8870ca8da0f0789abe214c3e1c992903ec
Relebohile.P <r.pheko@alustudent.com>
2024-03-21 18:59:13 +0000

    What is my status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://alu-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    expected_content = "Custom status"
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(r.text), r.text))
+    print("Received output matches the expected output." if r.text.strip() == expected_content else "Received output does not match the expected output.")
