c1434f238cdf0db01488233eb279e2921c56fe74
Relebohile.P <r.pheko@alustudent.com>
2024-03-24 14:29:50 +0000

    Curl Headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes URL as arg sends GET request with additional info
+# Send a GET request to a given URL with a header variable.
 curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
