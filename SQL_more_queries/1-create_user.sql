2f8eb48a2ac992994fe28500d7fdfb36cbb6c7d1
Relebohile.P <r.pheko@alustudent.com>
2024-03-24 11:17:59 -0700

    creates the MySQL server user user_0d_1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
--- Creates the MySQL server user user_0d_1 with all privileges
+-- Creates the MySQL server user user_0d_1 with all privileges if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Display the grants for the user user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
