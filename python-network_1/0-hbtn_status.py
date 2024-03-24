f35bc702058366bf3f06f44812280eea34e5fc70
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-03-24 23:15:57 +0200

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status."""
+"""This script retrieves the status of the intranet.hbtn.io
+website by making an HTTP GET request
+and prints details about the response body."""
+
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+if _name_ == '_main_':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
