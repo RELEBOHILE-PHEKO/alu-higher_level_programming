00e089298ada538f0f69d22bcd69888d346e9334
Relebohile.P <r.pheko@alustudent.com>
2024-03-24 10:58:46 +0000

    Update my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+
 """A script that
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
+
+- fetches content from two URLs simultaneously.
+- uses urllib package
+
 """
 
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
+    urls = ['https://alu-intranet.hbtn.io/status', 'http://0.0.0.0:5050/status']
 
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status, http://0.0.0.0:5050/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    for url in urls:
+        try:
+            with urllib.request.urlopen(url) as res:
+                content = res.read()
+                print("Body response for {}:".format(url))
+                print("\t- type: {}".format(type(content)))
+                print("\t- content: {}".format(content))
+                print("\t- utf8 content: {}".format(content.decode('utf-8')))
+                print()
+        except urllib.error.URLError as e:
+            print("Error fetching {}: {}".format(url, e.reason))
