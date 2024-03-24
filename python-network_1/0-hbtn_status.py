a5b4915b10834f9d9000d709891ed8db93bba3dd
Relebohile.P <r.pheko@alustudent.com>
2024-03-24 10:31:12 +0000

    Update my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """A script that
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
+- fetches https://intranet.hbtn.io/status and http://0.0.0.0:5050/status
+- uses urllib package
 """
 
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('http://0.0.0.0:5050/status') as res:
+def fetch_status(url):
+    with urllib.request.urlopen(url) as res:
         content = res.read()
-        print("Body response:")
+        print("Body response for {}:".format(url))
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print()
+
+if __name__ == '__main__':
+    urls = ['https://alu-intranet.hbtn.io/status', 'http://0.0.0.0:5050/status']
+    for url in urls:
+        fetch_status(url)
