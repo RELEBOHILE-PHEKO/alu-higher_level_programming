c04ebb397747d1a8f9abf3797f46be409cf25518
Relebohile.P <r.pheko@alustudent.com>
2024-04-02 12:06:18 -0700

    List all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""
+Script to list all states from the database 'hbtn_0e_0_usa'
+
+This script connects to a MySQL server running on 'localhost' at port '3306'
+and retrieves all rows from the 'states' table in the 'hbtn_0e_0_usa' database.
+The results are sorted in ascending order by the 'id' column and displayed on
+the console.
+
+Usage:
+    ./script.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    mysql_username: The username to connect to the MySQL server.
+    mysql_password: The password associated with the MySQL username.
+    database_name: The name of the MySQL database to connect to.
+"""
 import MySQLdb
 import sys
 
