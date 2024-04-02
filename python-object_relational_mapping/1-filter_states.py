571b69719bb87474b35088c66ca6c113bca4f42e
Relebohile.P <r.pheko@alustudent.com>
2024-04-02 12:15:52 -0700

    Filter statees by user input

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that lists all states with a name starting with 'N' from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user, pwd, db = sys.argv[1:4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=pwd, db=db)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+    cur.close()
+    conn.close()
