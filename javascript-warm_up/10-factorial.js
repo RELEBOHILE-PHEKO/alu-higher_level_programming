ace5c4fe16b591bccbc01426c5f5d6cfb6e5202e
Relebohile.P <r.pheko@alustudent.com>
2024-05-31 07:57:53 -0700

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
-function factorial(n) {
-  if (isNaN(n) || n === 0) {
-    return 1;
-  } else if (n < 0) {
-    return -1;
-  } else {
-    return n * factorial(n - 1);
-  }
+function factorial (n) { // Add a space before the parentheses
+  if (isNaN(n) || n < 0) return 1;
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 }
-
-const arg = parseInt(process.argv[2]);
-const result = factorial(arg);
-
-console.log(isFinite(result) ? result : 'Infinity');
+console.log(factorial(parseInt(process.argv[2])));
